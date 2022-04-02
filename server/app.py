bcc763c82e1559554a681bba9e34b819f3438b23
Shreyas Kaundinya <shreyassk08@gmail.com>
2022-04-02 13:58:33 +0530

    Added API endpoint to upload data

---
+++ server/app.py
@@ -0,0 +1,34 @@
+import json
+import os
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+from flask import Flask, Response, request
+
+app = Flask(__name__)
+
+PROJECT_ID = os.environ.get('PROJECT_ID')
+# Use the application default credentials
+cred = credentials.Certificate('./serviceAccountKey.json')
+firebase_admin.initialize_app(cred, {
+  'projectId': PROJECT_ID,
+})
+
+db = firestore.client()
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if (request.method == 'POST'):
+        data = request.get_json()
+
+        # validate data
+        if (not data.get('rfid') or not data.get('laser') or not data.get('force')):
+            return Response(status=400)
+        else :
+            # upload data to db
+            db.collection(u'sensordata').document().set(data)         
+            return Response(json.dumps(data), mimetype='application/json', status=200)
+    return 'NOT POST'
+
+
